553d2ed7fae6e666469bc0a8d2bb556071ac6378
dx <dx@example.com>
2025-10-02 07:35:48 -0300

    disko changes

--- hosts/Quanta/disko-config.nix
+++ hosts/Quanta/disko-config.nix
@@ -8,26 +8,22 @@
           type = "gpt";
           partitions = {
             ESP = {
-	            label = "boot";
               name = "ESP";
               size = "1G";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
+                extraArgs = [ "-n boot" ];
                 mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
               };
             };
             luks = {
               size = "100%";
-              label = "luks";
               content = {
                 type = "luks";
                 name = "cryptroot";
-                extraFormatArgs = [ "--type luks2" ];
+                askPassword = true;
                 settings = {
                   allowDiscards = true;
                   crypttabExtraOpts = [ "fido2-device=auto" ];
@@ -35,39 +31,45 @@
                 content = {
                   type = "btrfs";
                   extraArgs = [ "-L nixos" ];
-                  preUnmountHook = ''
-		    MNTPOINT=/mnt        
+                  postCreateHook = ''
+                    TMP_MNT=$(mktemp -d)
 
-                    mkdir -p $MNTPOINT/root/boot
-                    mkdir -p $MNTPOINT/root/nix
-                    mkdir -p $MNTPOINT/root/snapshots
-                    mkdir -p $MNTPOINT/root/persist
-                    mkdir -p $MNTPOINT/root/persist/local
-                    mkdir -p $MNTPOINT/root/persist/safe
+                    mount "$device" "$TMP_MNT"
 
-                    mkdir -p /mnt/snapshots/root
-                    btrfs subvolume snapshot -r $MNTPOINT/root $MNTPOINT/snapshots/root/blank
+                    mkdir -p "$TMP_MNT/root/boot"
+                    mkdir -p "$TMP_MNT/root/nix"
+                    mkdir -p "$TMP_MNT/root/snapshots"
+                    mkdir -p "$TMP_MNT/root/persist"
+                    mkdir -p "$TMP_MNT/root/persist/local"
+                    mkdir -p "$TMP_MNT/root/persist/safe"
+
+                    mkdir -p "$TMP_MNT/snapshots/root"
+
+                    btrfs subvolume snapshot -r "$TMP_MNT/root" "$TMP_MNT/snapshots/root/blank"
+
+                    umount "$TMP_MNT"
+                    rmdir "$TMP_MNT"
                   '';
                   subvolumes = {
                     "/root" = {
                       mountpoint = "/";
-                      mountOptions = [ "subvol=root" "noatime" "nodiratime" "ssd" ];
+                      mountOptions = [ "noatime", "nodiratime", "ssd" ];
                     };
                     "/nix" = {
                       mountpoint = "/nix";
-                      mountOptions = [ "subvol=nix" "noatime" "nodiratime" "ssd" "compress=zstd" ];
+                      mountOptions = [ "noatime", "nodiratime", "ssd", "compress=zstd" ];
                     };
                     "/snapshots" = {
                       mountpoint = "/snapshots";
-                      mountOptions = [ "subvol=snapshots" "noatime" "nodiratime" "ssd" ];
+                      mountOptions = [ "noatime", "nodiratime", "ssd" ];
                     };
                     "/persist_local" = {
                       mountpoint = "/persist/local";
-                      mountOptions = [ "subvol=persist_local" "noatime" "nodiratime" "ssd" ];
+                      mountOptions = [ "noatime", "nodiratime", "ssd" ];
                     };
                     "/persist_safe" = {
                       mountpoint = "/persist/safe";
-                      mountOptions = [ "subvol=persist_safe" "noatime" "nodiratime" "ssd" ];
+                      mountOptions = [ "noatime", "nodiratime", "ssd" ];
                     };
                   };
                 };
@@ -82,5 +84,5 @@
   fileSystems = {
     "/persist/local".neededForBoot = true;
     "/persist/safe".neededForBoot = true;
-  };  
+  };
 }
